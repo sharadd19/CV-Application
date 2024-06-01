7bdb94d838308db5e9922714f3d39726f294d4e3
Sharad <sharadd19@gmail.com>
2024-06-01 15:07:19 +1000

    added space between titles and expanded toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Skills from "./components/Skills";
 import Cv from "./components/CV";
 import "./App.css";
 import { v4 as uuid4 } from "uuid";
-import JsPDF from 'jspdf';
+import JsPDF from "jspdf";
 
 function App() {
   const [personalInformation, setPersonalInformation] = useState({
@@ -60,79 +60,84 @@ function App() {
   }
 
   function downloadCv() {
-    const report = new JsPDF('portrait', 'pt', 'a4');
-    report.html(document.querySelector('.cvContainer')).then(() => {
-      report.save('report.pdf');
+    const report = new JsPDF("portrait", "pt", "a4");
+    report.html(document.querySelector(".cvContainer")).then(() => {
+      report.save("report.pdf");
     });
   }
 
   return (
     <>
-    <div className="mainTitle"><h1>Build your CV</h1></div>
-    <div className="wrapper">
-      
-      <div className="mainContainer">
-        
-        <button className="download" onClick={downloadCv}>Download</button>
-        
-        <div className="personalInformationInput">
-          <div>
-            <button className="toggle" onClick={togglePersonalInformation}>
-              <h2>Personal Details{<span>{showPersonalInformation ? "▲" : "▼"}</span>}</h2>
-            </button>
-          </div>
-          {showPersonalInformation && (
-            <PersonalInformation
-              personalInformation={personalInformation}
-              setPersonalInformation={setPersonalInformation}
-              showPersonalInformation={showPersonalInformation}
-              setShowPersonalInformation={setShowPersonalInformation}
-            />
-          )}
-        </div>
+      <div className="mainTitle">
+        <h1>Build your CV</h1>
+      </div>
+      <div className="wrapper">
+        <div className="mainContainer">
+          <button className="download" onClick={downloadCv}>
+            Download
+          </button>
 
-        <div className="educationInput">
-          <div>
-            <button className="toggle" onClick={toggleEducation}>
-              <h2>Education{<span>{showEducation ? "▲" : "▼"}</span>}</h2>
-            </button>
+          <div className="personalInformationInput">
+            <div>
+              <button className="toggle" onClick={togglePersonalInformation}>
+                <h2>
+                  Personal Details&nbsp;
+                  {<span>{showPersonalInformation ? "▲" : "▼"}</span>}
+                </h2>
+              </button>
+            </div>
+            {showPersonalInformation && (
+              <PersonalInformation
+                personalInformation={personalInformation}
+                setPersonalInformation={setPersonalInformation}
+                showPersonalInformation={showPersonalInformation}
+                setShowPersonalInformation={setShowPersonalInformation}
+              />
+            )}
           </div>
-          {showEducation && (
-            <Education
-              educations={educations}
-              setEducation={setEducation}
-              showEducation={showEducation}
-              setShowEducation={setShowEducation}
-            />
-          )}
-        </div>
 
-        <div className="workExperienceInput">
-          <div>
-            <button className="toggle" onClick={toggleWorkExperience}>
-              <h2>
-                Work Experience{<span>{showWorkExperience ? "▲" : "▼"}</span>}
-              </h2>
-            </button>
+          <div className="educationInput">
+            <div>
+              <button className="toggle" onClick={toggleEducation}>
+                <h2>Education&nbsp;{<span>{showEducation ? "▲" : "▼"}</span>}</h2>
+              </button>
+            </div>
+            {showEducation && (
+              <Education
+                educations={educations}
+                setEducation={setEducation}
+                showEducation={showEducation}
+                setShowEducation={setShowEducation}
+              />
+            )}
           </div>
-          {showWorkExperience && (
-            <WorkExperience
-              workExperiences={workExperiences}
-              setWorkExperience={setWorkExperience}
-              showWorkExperience={showWorkExperience}
-              setShowWorkExperience={setShowWorkExperience}
-            />
-          )}
+
+          <div className="workExperienceInput">
+            <div>
+              <button className="toggle" onClick={toggleWorkExperience}>
+                <h2>
+                  Work Experience&nbsp;{<span>{showWorkExperience ? "▲" : "▼"}</span>}
+                </h2>
+              </button>
+            </div>
+            {showWorkExperience && (
+              <WorkExperience
+                workExperiences={workExperiences}
+                setWorkExperience={setWorkExperience}
+                showWorkExperience={showWorkExperience}
+                setShowWorkExperience={setShowWorkExperience}
+              />
+            )}
+          </div>
+        </div>
+        <div className="cvContainer">
+          <Cv
+            personalInformation={personalInformation}
+            educations={educations}
+            workExperiences={workExperiences}
+          />
         </div>
       </div>
-      <div className="cvContainer">
-        <Cv
-          personalInformation={personalInformation}
-          educations={educations}
-          workExperiences={workExperiences}
-        />
-      </div>
-    </div>
     </>
   );
 }
